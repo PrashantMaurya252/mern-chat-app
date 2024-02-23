23b1331016004c7553c34bedb6a9356f26542baf
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-02-23 09:43:46 +0530

    creating middleware and login endPoints

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import bcryptjs from "bcryptjs";
+import generateToken from "../utils/generateTokens.js";
 
 export const signupUser = async (req, res) => {
   try {
@@ -16,7 +17,7 @@ export const signupUser = async (req, res) => {
     }
 
     const salt = await bcryptjs.genSalt(10);
-    const hashedPassword = await bcryptjs.hash(password,salt)
+    const hashedPassword = await bcryptjs.hash(password, salt);
 
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`;
     const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`;
@@ -24,22 +25,23 @@ export const signupUser = async (req, res) => {
     const newUser = new User({
       fullName,
       userName,
-      password:hashedPassword,
+      password: hashedPassword,
       gender,
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
 
-    if(newUser){
-        await newUser.save();
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
 
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      userName: newUser.userName,
-      gender: newUser.gender,
-    });
-    }else{
-        return res.status(400).json({error:"Invalid User Data"})
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        userName: newUser.userName,
+        gender: newUser.gender,
+      });
+    } else {
+      return res.status(400).json({ error: "Invalid User Data" });
     }
   } catch (error) {
     console.log("error in signUp controller", error.message);
@@ -47,8 +49,27 @@ export const signupUser = async (req, res) => {
   }
 };
 
-export const loginUser = (req, res) => {
-  console.log("Login User");
+export const loginUser = async (req, res) => {
+  try {
+    const { userName, password } = req.body;
+    const user = await User.findOne({ userName });
+    const isPasswordCorrect = await bcryptjs.compare(password,user?.password || "")
+    if(!user || !isPasswordCorrect){
+      return res.status(400).json({error:"Invalid Credentials"})
+    }
+
+    generateToken(user._id,res);
+
+    res.status(200).json({
+      _id:user._id,
+      fullName:user.fullName,
+      userName:user.userName,
+      profilePic:user.profilePic
+    })
+  } catch (error) {
+    console.log("error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export const logoutUser = (req, res) => {
