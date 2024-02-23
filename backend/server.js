def776b6a58ba3aef8e0c16f7a89292ae9d1e961
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-02-23 14:12:35 +0530

    protect Route middleware

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes.js'
+import authRoutes from './routes/authRoutes.js';
+import messageRoutes from './routes/messageRoutes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
+import cookieParser from 'cookie-parser';
 
 const app=express();
 dotenv.config()
@@ -11,7 +13,10 @@ app.get('/',(req,res)=>{
     res.send('Hello World!!')
 })
 app.use(express.json())  // to parse the incoming request with JSON payloads(from req.body)
+app.use(cookieParser())
 app.use('/api/auth',authRoutes)
+
+app.use('/api/messages',messageRoutes)
 
 app.listen(PORT,()=>{
     connectToMongoDB()
