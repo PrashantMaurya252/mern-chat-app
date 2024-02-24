e9438b38441ce0d0dcc862aea32651a5255d2383
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-02-24 13:13:45 +0530

    building UI

---
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -0,0 +1,25 @@
+const Conversation = () => {
+  return (
+    <>
+      <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
+        <div className="avatar online">
+          <div className="w-14 h-14 rounded-full">
+            <img
+              src="https://images.pexels.com/photos/3343253/pexels-photo-3343253.jpeg?auto=compress&cs=tinysrgb&"
+              alt="user avatar"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col flex-1">
+          <div className="flex gap-3 justify-between">
+            <p className="font-bold text-gray-200">John Doe</p>
+            <span className="text-xl">😎</span>
+          </div>
+        </div>
+      </div>
+      <div className="divider my-0 py-0 h-1" />
+    </>
+  );
+};
+
+export default Conversation;
