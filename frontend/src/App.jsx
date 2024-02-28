800daaad0a1dd56c7117d205d1ba6d07913974db
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-02-28 12:10:11 +0530

    get all the conversation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   const {authUser}=useAuthContext()
   return <div className="h-screen p-4 flex items-center justify-center">
     <Routes>
-      <Route path="/" element={authUser ? <Home/>:<Navigate to='/'/>}/>
+      <Route path="/" element={authUser ? <Home/>:<Navigate to='/login'/>}/>
       <Route path="/login" element={authUser ? <Navigate to='/'/>:<Login/>}/>
       <Route path="/signup" element={authUser ? <Navigate to='/'/>:<Signup/>}/>
     </Routes>
