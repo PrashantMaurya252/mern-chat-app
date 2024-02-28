a97810c471809e09fd521aa2635d208d4cb10b65
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-02-28 12:53:43 +0530

    send message functionality

--- frontend/src/components/messages/MessageInput.jsx
+++ frontend/src/components/messages/MessageInput.jsx
@@ -1,19 +1,32 @@
+import { useState } from "react";
 import { BiSolidSend } from "react-icons/bi";
+import useSendMesage from "../../hooks/useSendMesage";
 
 const MessageInput = () => {
+  const [message, setMessage] = useState("");
+  const { loading, sendMessage } = useSendMesage();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!message) return;
+    await sendMessage(message);
+    setMessage("")
+  };
   return (
-    <form>
+    <form className="px-4 my-3" onSubmit={handleSubmit}>
       <div className="w-full relative">
         <input
           type="text"
           placeholder="Send a message"
           className="border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white"
+          value={message}
+          onChange={(e)=>setMessage(e.target.value)}
         />
         <button
           type="submit"
           className="absolute inset-y-0 end-0 flex items-center pe-3"
         >
-          <BiSolidSend />
+          {loading ? <div className="loading loading-spinner"></div>:<BiSolidSend />}
         </button>
       </div>
     </form>
