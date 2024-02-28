79aa5586137f4457c2b459e11fa5bde04f33b836
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-02-28 14:10:17 +0530

    search Input functionality

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,16 +1,23 @@
-import Message from "./Message"
-
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeletons from "../skeletons/MessageSkeletons";
+import Message from "./Message";
 
 const Messages = () => {
+  const { messages, loading } = useGetMessages();
+
   return (
     <div className="px-4 flex-1 overflow-auto">
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <Message key={message._id} message={message} />
+        ))}
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeletons key={idx} />)}
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Send a Message to start a conversation</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Messages
+export default Messages;
