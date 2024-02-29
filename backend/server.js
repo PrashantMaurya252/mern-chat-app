9bfe8144d4336e447712060f7a882739942ca7f1
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-02-29 09:34:25 +0530

    search functionality

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,13 @@ import messageRoutes from './routes/messageRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 import cookieParser from 'cookie-parser';
+import { app, server } from './socket/socket.js';
 
-const app=express();
+
 dotenv.config()
 const PORT=process.env.PORT || 5000
 
-app.get('/',(req,res)=>{
-    res.send('Hello World!!')
-})
+
 app.use(express.json())  // to parse the incoming request with JSON payloads(from req.body)
 app.use(cookieParser())
 app.use('/api/auth',authRoutes)
@@ -20,6 +19,6 @@ app.use('/api/users',userRoutes)
 
 app.use('/api/messages',messageRoutes)
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     connectToMongoDB()
     console.log(`App is running on ${PORT}`)})
