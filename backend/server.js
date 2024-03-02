3242661c2e44bf432a0ac800e5d22c0e622c58d8
PrashantMaurya252 <mauryaprashant252@gmail.com>
2024-03-02 09:46:16 +0530

    completed

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,32 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes.js';
-import messageRoutes from './routes/messageRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import connectToMongoDB from './db/connectToMongoDB.js';
-import cookieParser from 'cookie-parser';
-import { app, server } from './socket/socket.js';
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import cookieParser from "cookie-parser";
+import { app, server } from "./socket/socket.js";
 
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
-dotenv.config()
-const PORT=process.env.PORT || 5000
+const __dirname = path.resolve();
 
+app.use(express.json()); // to parse the incoming request with JSON payloads(from req.body)
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
-app.use(express.json())  // to parse the incoming request with JSON payloads(from req.body)
-app.use(cookieParser())
-app.use('/api/auth',authRoutes)
-app.use('/api/users',userRoutes)
+app.use("/api/messages", messageRoutes);
 
-app.use('/api/messages',messageRoutes)
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
 
-server.listen(PORT,()=>{
-    connectToMongoDB()
-    console.log(`App is running on ${PORT}`)})
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
+
+server.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`App is running on ${PORT}`);
+});
